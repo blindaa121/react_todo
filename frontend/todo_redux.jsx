63fb5050978407fc978a4c8c9bfce8e3b362ce79
Bryan <blindaa121@gmail.com>
2020-06-15 09:28:01 -0700

    first commit

---
+++ frontend/todo_redux.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+// import root from './components/root'
+
+document.addEventListener("DOMContentLoaded", () => {
+    const root = document.getElementById("content")
+    ReactDOM.render(<h1>Todos App</h1>, root)
+});
