33cb396ae7dd9afd3ecf86990009c98aa9b853b3
Bryan <blindaa121@gmail.com>
2020-06-15 13:25:14 -0700

    Create selectors

---
+++ frontend/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <h1>Knock out those daily tasks!</h1>
+    )
+}
+
+export default App;
--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom'
 // import root from './components/root'
 import configureStore from './store/store';
 import { receiveTodo, receiveTodos } from './actions/todo_actions'
-
+import Root from './components/root'
 
 document.addEventListener("DOMContentLoaded", () => {
-    const root = document.getElementById("content")
-    ReactDOM.render(<h1>Todos App</h1>, root)
-
     const store = configureStore();
+    const root = document.getElementById("content")
+    ReactDOM.render(<Root store={store}/>, root)
     window.store = store
     window.receiveTodo = receiveTodo;
     window.receiveTodos = receiveTodos;
